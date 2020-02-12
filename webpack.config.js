0991cc93dccaafb78ed102c08da354a86a259124
15099836861 <562172151@qq.com>
2020-02-12 16:37:30 +0800

    启用HMR

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,27 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpack = require('webpack');
 module.exports = {
     entry: {
-        app:'./src/index.js',
-        print:'./src/print.js'
-    }, 
-    devtool:'inline-source-map',
-    plugins:[
+        app: './src/index.js'
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+        hot: true
+    },
+    plugins: [
         new CleanWebpackPlugin({}),
         new HtmlWebpackPlugin(
-            {title:'output Management'}
-        )
-    ], 
+            { title: 'output Management' }
+        ),
+        new webpack.NamedModulesPlugin(),
+        new webpack.HotModuleReplacementPlugin()
+    ],
     output: {
         filename: '[name].bundle3.js',
         path: path.resolve(__dirname, 'dist'),
-        publicPath:'/'
+        publicPath: '/'
     }
 }
