b4d78aba9318839be5c9f24b840357dbad41fd57
15099836861 <562172151@qq.com>
2020-02-13 09:45:46 +0800

    minification source map  指定环境

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,9 +1,27 @@
+const path = require("path")
+const webpack = require('webpack');
 const merge = require('webpack-merge');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.common.js');
-
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 module.exports = merge(common, {
-  plugins: [
-    new UglifyJSPlugin()
-  ]
+    entry:{
+        app: './src/index.js'
+    },
+    output:{
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/'
+    },
+    devtool: 'source-map',
+    plugins: [
+        new CleanWebpackPlugin({}),
+        new UglifyJSPlugin({
+            sourceMap: true
+        }),
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('production')
+        })
+    ],
+    mode:"production"
 });
