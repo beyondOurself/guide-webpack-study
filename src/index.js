7c960bb8819dd0f83cf79047443a7c08a9627cbe
15099836861 <562172151@qq.com>
2020-02-13 16:19:57 +0800

    shimming 全局变量

--- src/index.js
+++ src/index.js
@@ -1,18 +1,7 @@
 
-import _ from 'lodash';
-import printMe from './print.js';
 function component() {
   var element = document.createElement('div');
-  var btn = document.createElement('button');
-
-  // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
-  // lodash ,now imported by this script 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe;
-  element.appendChild(btn);
-
+  element.innerHTML = _.join(['777', 'webpack'], ' ');
   return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,30 @@
 const path = require('path');
+const webpack = require('webpack');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
 module.exports = {
     entry: {
-        app:'./src/index.js',
-        print:'./src/print.js'
-    }, 
-    plugins:[
+        app: './src/index.js',
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+        hot: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'shimming'
+        }),
         new CleanWebpackPlugin({}),
-        new HtmlWebpackPlugin(
-            {title:'output Management'}
-        )
-    ], 
+        new webpack.ProvidePlugin({
+            _: 'lodash'
+        }),
+        new webpack.NamedModulesPlugin(),
+        new webpack.HotModuleReplacementPlugin()
+    ],
     output: {
-        filename: '[name].bundle3.js',
+        filename: '[name].[hash].js',
         path: path.resolve(__dirname, 'dist')
     }
 }
