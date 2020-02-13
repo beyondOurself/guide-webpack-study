6daf4ecb2a3a3e76f6648eeb1e317ba4919200fa
15099836861 <562172151@qq.com>
2020-02-13 11:25:03 +0800

    防止重复

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
@@ -12,6 +13,21 @@ module.exports = {
             { title: 'Code Splitting' }
         )
     ],
+    //CommonsChunkPlugin 已经在webpack4.0移除 改用 splitChunksPlugin
+    optimization: {
+        runtimeChunk: {
+            name: "commons"
+        },
+        splitChunks: {
+            cacheGroups: {
+                commons: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: "vendor",
+                    chunks: "all"
+                }
+            }
+        }
+    },
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
